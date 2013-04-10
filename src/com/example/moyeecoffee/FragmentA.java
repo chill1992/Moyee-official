13c5ab524f4bbf08629defe7d8fc2aadf5cfa1e0
Mark <mhuigen1992@gmail.com>
2013-04-10 12:53:51 +0200

    Importeren code

---
+++ src/com/example/moyeecoffee/FragmentA.java
@@ -0,0 +1,28 @@
+package com.example.moyeecoffee;
+
+import com.example.moyeecoffee.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class FragmentA extends Fragment 
+{
+	Button button;
+
+@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle saved)
+	{
+		return inflater.inflate(R.layout.frag_a, group, false);
+	}
+
+@Override
+	public void onActivityCreated (Bundle savedInstanceState)
+	{
+		super.onActivityCreated(savedInstanceState);
+		
+	}
+}
---
+++ src/com/example/moyeecoffee/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.moyeecoffee;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class MainActivity extends SherlockFragmentActivity {
+
+/** Called when the activity is first created. */
+@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		ActionBar bar = getSupportActionBar();
+		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		ActionBar.Tab tab1 = bar.newTab();
+		ActionBar.Tab tab2 = bar.newTab();
+		ActionBar.Tab tab3 = bar.newTab();
+		tab1.setText("Locaties");
+		tab2.setText("Bestellingen");
+		tab3.setText("Koffies");
+		tab1.setTabListener(new MyTabListener());
+		tab2.setTabListener(new MyTabListener());
+		tab3.setTabListener(new MyTabListener());
+		bar.addTab(tab1);
+		bar.addTab(tab2);
+		bar.addTab(tab3);
+	}
+
+	private class MyTabListener implements ActionBar.TabListener
+	{
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			if(tab.getPosition()==0)
+			{
+				FragmentA frag = new FragmentA();
+				ft.replace(android.R.id.content, frag);
+			}
+			if(tab.getPosition()==1)
+			{
+				FragmentB frag = new FragmentB();
+				ft.replace(android.R.id.content, frag);
+			}
+			if(tab.getPosition()==2)
+			{
+				FragmentC frag = new FragmentC();
+				ft.replace(android.R.id.content, frag);
+			}
+	}
+		
+@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	// TODO Auto-generated method stub
+	}
+
+@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	// TODO Auto-generated method stub
+	}
+}
+}
